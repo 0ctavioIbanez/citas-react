5d4402aeccc6678e74793b88dccae1453956a273
Octavio Ibanez <octaviogd@live.com.mx>
2020-09-15 20:45:51 -0500

    Updates

---
+++ precache-manifest.68f4a9809bda972037fa70762d4b49ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "882838cdf2665f73b8f259c6495b94b0",
+    "url": "/citas-react/index.html"
+  },
+  {
+    "revision": "0f0c5ba26c0df27903dd",
+    "url": "/citas-react/static/css/main.bb18ebbb.chunk.css"
+  },
+  {
+    "revision": "d26772214550f200a729",
+    "url": "/citas-react/static/js/2.937a4f6c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/citas-react/static/js/2.937a4f6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f0c5ba26c0df27903dd",
+    "url": "/citas-react/static/js/main.67b9b6b2.chunk.js"
+  },
+  {
+    "revision": "7f27d902abc63ad605bb",
+    "url": "/citas-react/static/js/runtime-main.f63db2f0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/citas-reactn/precache-manifest.fbe6548cd2f26c76ee6a14fa33680048.js"
+  "/citas-react/precache-manifest.68f4a9809bda972037fa70762d4b49ed.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/citas-reactn/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/citas-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
